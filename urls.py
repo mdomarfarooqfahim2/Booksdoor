31897b2c166a7c03ee3127ae0045db42f200dae6
mdomarfarooqfahim2 <168060652+mdomarfarooqfahim2@users.noreply.github.com>
2024-04-25 11:48:58 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path
+
+from . import views
 
 
+app_name = 'conversation'
 
+urlpatterns = [
+    path('', views.inbox, name='inbox'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('new/<int:item_pk>/', views.new_conversation, name='new'),
 
-urlpatterns =[
-    path("inbox/", include('conversation.urls')),
-    path("", include('core.urls')),
-    path('items/', include('Item.urls')),
-    path('dashboard/', include('dashboard.urls')),
-
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
+] 
